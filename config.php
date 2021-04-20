b3f326e87a546c2fbbfbfe0f0c2975429f56a2bf
Rachna Soundatti <43413401+rachnasoundatti@users.noreply.github.com>
2021-04-20 12:42:13 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
 /*Configuration Settings*/
 
-define('DB_HOST', 'localhost'); /*Database Server*/
-define('DB_NAME', 'CRUD'); /*Database Name*/
-define('DB_USER', 'root'); /*Database Username*/
-define('DB_PWD',  ''); /*Database Password*/
+define('DB_HOST', 'remotemysql.com'); /*Database Server*/
+define('DB_NAME', 'EjuAQytIDk'); /*Database Name*/
+define('DB_USER', 'EjuAQytIDk'); /*Database Username*/
+define('DB_PWD',  'GuRRpZDOxP'); /*Database Password*/
+
 ?>
